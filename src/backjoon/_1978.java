32679e2cb9d95098473949ed564b9ce329c871c5
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-30 23:25:35 +0900

    No.11653 소수찾기

---
+++ src/backjoon/_1978.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class _1978 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int k = sc.nextInt();
+            boolean isPrime = true;
+
+            if (k == 1)
+                continue;
+            for (int j = 2; j < Math.sqrt(k); j++) {
+                if (k % j == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                count++;
+            }
+
+        }
+        System.out.println(count);
+    }
+}
