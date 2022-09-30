457aaaef378d4a8204712efa535739a2475473c9
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-30 23:31:40 +0900

    No.1978 소수찾기

--- src/backjoon/_1978.java
+++ src/backjoon/_1978.java
@@ -2,28 +2,27 @@ package backjoon;
 
 import java.util.Scanner;
 
+//주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
 public class _1978 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int isPrime;
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            int k = sc.nextInt();
-            boolean isPrime = true;
-
-            if (k == 1)
-                continue;
-            for (int j = 2; j < Math.sqrt(k); j++) {
-                if (k % j == 0) {
-                    isPrime = false;
+        for (int i = 0; i < num; i++) {
+            isPrime = input.nextInt();
+            for (int p = 2; p <= isPrime; p++) {
+                if (p == isPrime) {
+                    count++;
+                }
+                if (isPrime % p == 0) {
+                    break;
                 }
             }
-            if (isPrime) {
-                count++;
-            }
-
         }
         System.out.println(count);
     }
 }
+
