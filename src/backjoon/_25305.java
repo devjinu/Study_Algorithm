9f7a0ef8589d66a0621ab04ce501f4d967dd4fc9
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-10-01 13:36:51 +0900

    No.25305 커트라인

--- src/backjoon/_25305.java
+++ src/backjoon/_25305.java
@@ -1,5 +1,9 @@
 package backjoon;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 //2022 연세대학교 미래캠퍼스 슬기로운 코딩생활에 $N$명의 학생들이 응시했다.
 //이들 중 점수가 가장 높은 $k$명은 상을 받을 것이다. 이 때, 상을 받는 커트라인이 몇 점인지 구하라.
 //커트라인이란 상을 받는 사람들 중 점수가 가장 가장 낮은 사람의 점수를 말한다.
@@ -7,13 +11,10 @@ package backjoon;
 //첫째 줄에는 응시자의 수 $N$과 상을 받는 사람의 수 $k$가 공백을 사이에 두고 주어진다.
 //둘째 줄에는 각 학생의 점수 $x$가 공백을 사이에 두고 주어진다.
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-
 // 1 <= N <= 1000
 // 1 <= k <= N
 // 0 <= x <= 10000
+
 public class _25305 {
     public static void main(String[] args) {
 
@@ -31,7 +32,6 @@ public class _25305 {
         Arrays.sort(arr, Collections.reverseOrder());
 
         System.out.println(arr[k-1]);
-
 
     }
 }
