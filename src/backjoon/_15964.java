ac6c94c9578d0160b998171d617f3a490a4bbd89
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-10-17 22:22:40 +0900

    No.15964 이상한 기호

--- src/backjoon/_15964.java
+++ src/backjoon/_15964.java
@@ -1,5 +1,7 @@
 package backjoon;
 
+import java.util.Scanner;
+
 //부산일과학고등학교의 효진이는 수학의 귀재이다. 어떤 문제라도 보면 1분 내에 풀어버린다는 학교의 전설이 내려올 정도였는데, 이런 킹ㅡ갓 효진에게도 고민이 생겼다.
 // 대부분의 문제에서 반복되는 연산이 있었기 때문이다! 이 연산은 너무 길어서 종이에 풀던 효진이는 너무 고통스러워서, 자신이 새로 연산자를 만들기로 했다.
 //연산자의 기호는 ＠으로, A＠B = (A+B)×(A-B)으로 정의내리기로 했다.
@@ -9,6 +11,9 @@ package backjoon;
 // 첫째 줄에 A＠B의 결과를 출력한다.
 public class _15964 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println((a+b)*(a-b));
     }
 }
