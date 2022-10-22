43f7f76e9194b547d3551676beee46b4f497c81e
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-10-22 21:23:53 +0900

    No.2839 설탕 배달

--- src/backjoon/_2839.java
+++ src/backjoon/_2839.java
@@ -1,4 +1,7 @@
 package backjoon;
+
+import java.util.Scanner;
+
 //상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다.
 // 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
 //상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만,
@@ -8,7 +11,23 @@ package backjoon;
 //상근이가 배달하는 봉지의 최소 개수를 출력한다. 만약, 정확하게 N킬로그램을 만들 수 없다면 -1을 출력한다.
 public class _2839 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int N = sc.nextInt();
+
+        // N이 4 or 7 -> N킬로그램 x
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        }
+        else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        }
+        else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        }
+        else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
     }
 
 }
