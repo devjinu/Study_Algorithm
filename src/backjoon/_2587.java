ff7c586b6bc000624ec8e84a858eba22cfb19f74
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-10-28 21:41:22 +0900

    No.2587 대표값2

--- src/backjoon/_2587.java
+++ src/backjoon/_2587.java
@@ -1,5 +1,8 @@
 package backjoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 //어떤 수들이 있을 때, 그 수들을 대표하는 값으로 가장 흔하게 쓰이는 것은 평균이다. 평균은 주어진 모든 수의 합을 수의 개수로 나눈 것이다.
 // 예를 들어 10, 40, 30, 60, 30의 평균은 (10 + 40 + 30 + 60 + 30) / 5 = 170 / 5 = 34가 된다.
 //평균 이외의 또 다른 대표값으로 중앙값이라는 것이 있다. 중앙값은 주어진 수를 크기 순서대로 늘어 놓았을 때 가장 중앙에 놓인 값이다. 예를 들어 10, 40, 30, 60, 30의 경우, 크기 순서대로 늘어 놓으면
@@ -9,6 +12,16 @@ package backjoon;
 //첫째 줄에는 평균을 출력하고, 둘째 줄에는 중앙값을 출력한다. 평균과 중앙값은 모두 자연수이다.
 public class _2587 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[5];
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
 
+        Arrays.sort(arr);
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
     }
 }
