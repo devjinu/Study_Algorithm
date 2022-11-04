f1218313b0606ecf77490ad7418e3ee6f4dc6dde
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-11-04 21:51:37 +0900

    No.2164 카드2

--- src/backjoon/_2000/_2164.java
+++ src/backjoon/_2000/_2164.java
@@ -1,5 +1,9 @@
 package backjoon._2000;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 //N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
 //이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 우선, 제일 위에 있는 카드를 바닥에 버린다. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
 //예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다.
@@ -9,7 +13,21 @@ package backjoon._2000;
 //첫째 줄에 남게 되는 카드의 번호를 출력한다.
 public class _2164 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int N = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            q.offer(q.poll());
+        }
+        System.out.println(q.poll());
     }
 
 }
