8c08f8ce9b7cc492383c35e76b2ff71068259cc0
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-11-26 21:01:28 +0900

    No.2558 A+B-2

--- src/backjoon/_2000/_2558.java
+++ src/backjoon/_2000/_2558.java
@@ -1,10 +1,15 @@
 package backjoon._2000;
 
+import java.util.Scanner;
+
 //두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 //첫째 줄에 A, 둘째 줄에 B가 주어진다. (0 < A, B < 10)
 //첫째 줄에 A+B를 출력한다.
 public class _2558 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
     }
 }
