17eac537f6d3956b2fb6bec20675aa03fada0a95
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-12-06 22:45:27 +0900

    No.11719 그대로 출력하기 2

--- src/backjoon/_11000/_11719.java
+++ src/backjoon/_11000/_11719.java
@@ -1,7 +1,13 @@
 package backjoon._11000;
 
+import java.util.Scanner;
+
 public class _11719 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()){
+            String result = sc.nextLine();
+            System.out.println(result);
+        }
     }
 }
