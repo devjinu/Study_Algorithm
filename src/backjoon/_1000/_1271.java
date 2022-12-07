9f4fc952dd0433fdd3f8e5191b07b52c9b5b5120
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-12-07 22:42:14 +0900

    No.1271 엄청난 부자2

--- src/backjoon/_1000/_1271.java
+++ src/backjoon/_1000/_1271.java
@@ -1,5 +1,8 @@
 package backjoon._1000;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 //갑부 최백준 조교는 동전을 최소로 바꾸는데 성공했으나 김재홍 조교가 그 돈을 발견해서 최백준 조교에게 그 돈을 나누자고 따진다.
 //그 사실이 전 우주로 알려지자 우주에 있던 많은 생명체들이 자신들에게 돈을 분배해 달라고 당장 달려오기 시작했다.
 //프로토스 중앙 우주 정부의 정책인, ‘모든 지적 생명체는 동등하다’라는 규칙에 입각해서 돈을 똑같이 분배하고자 한다.
@@ -9,6 +12,11 @@ package backjoon._1000;
 //첫째 줄에 생명체 하나에게 돌아가는 돈의 양을 출력한다. 그리고 두 번째 줄에는 1원씩 분배할 수 없는 남는 돈을 출력한다.
 public class _1271 {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
 
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
     }
 }
